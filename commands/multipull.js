157e1c5b0a67c718fbc20a0d280a0e6e57bc5b1b
Fadil Ardiansyah <v@clude.id>
2020-09-19 04:57:29 +0700

    awawa

--- commands/multipull.js
+++ commands/multipull.js
@@ -2,7 +2,6 @@ const { settings } = require("cluster");
 const bot_config = require('../bot.config.json');
 const weapA = require('../assets/json/weaponA.json');
 const weapS = require('../assets/json/weaponS.json');
-const weapSR = require('../assets/json/weaponSR.json');
 const prefix = bot_config.bot_config.prefix;
 var uuid = require('uuid');
 var jimp = require('jimp');
@@ -16,7 +15,8 @@ let roll = function() {
     return chance.integer({ min: 1, max: 100 })
 }
 
-let pull = function() {
+let pull = function(rateup) {
+    const weapSR = require(rateup);
 
     let result = roll();
 
@@ -56,8 +56,8 @@ let pull = function() {
         return randomValue;
 }
 
-let pullSpec = function() {
-
+let pullSpec = function(rateup) {
+    const weapSR = require(rateup);
     let result = roll();
 
     if(result >= 1 && result <= 25 ) {
@@ -89,95 +89,132 @@ let pullSpec = function() {
 }
 
 
-    
+let pullGuaranteed = function(rateup, guaranteed) {
+    const weapSR = require(rateup);
+    var animalArray  = Object.keys(weapSR);
+    var randomNumber = Math.random();
+    var animalIndex  = Math.floor(randomNumber * guaranteed.length);
+    var randomKey    = animalArray[animalIndex];
+    var randomValue  = weapSR[randomKey]; 
+    return randomValue;
+}
+
 
 
 module.exports = {
     name: 'multipull',
     description: 'tes hoky anda dengan multipull',
     execute(message, args, client) {
-        if (args.length === 0) {
 
-            let images = ['./assets/img/gambargacha.png']
-            let textE = [];
-            for(let i = 0; i < 11; i++){
-            
-
-                if(i == 10){
-                    a = pullSpec();
-                    if(a.rarity == 'SR'){
-                        textE.push(util.evalRarity(a.rarity,client) + a.name);
-                    }
-                    images.push(a.url);
-                    
-                }
-                else{
-                    a = pull();
-                    if(a.rarity == 'SR'){
-                        textE.push(util.evalRarity(a.rarity,client) + a.name);
-                    }
-                    images.push(a.url);
-                   
-          
-                }
-                // console.log(i);
-            }
-            
-            textE = (textE.length > 0 ? textE : 'n/a')
-
-            var jimps = [];
-            let theUrl = '';
-            for(let i = 0; i < images.length; i++){
-                jimps.push(jimp.read(images[i])); 
-            }
-           
-
-            Promise.all(jimps).then(function(data){
-                return Promise.all(jimps);
-            }).then(function(data){
-                data[0].composite(data[1],30,30);
-                data[0].composite(data[2],186,30);
-                data[0].composite(data[3],342,30);
-
-                data[0].composite(data[4],30,186);
-                data[0].composite(data[5],186,186);
-                data[0].composite(data[6],342,186);
-
-                data[0].composite(data[7],30,342);
-                data[0].composite(data[8],186,342);
-                data[0].composite(data[9],342,342);
-
-                data[0].composite(data[10],113,498);
-                data[0].composite(data[11],269,498);
-
-                imgName = uuid.v1() + '.png';
-                theUrl = './assets/img/your/yourgacha' + imgName;
-
-                data[0].write(theUrl, function(){
-                    // console.log(message.guild.members.cache.get(message.author.id));
-                    const user = message.guild.members.cache.get(message.author.id);
-                    let URLgambar = 'https://cdn.discordapp.com/avatars/'+ user.user.id + '/' + user.user.avatar + '.png?size=64';
-                    const attachment = new Discord
-                      .MessageAttachment(theUrl, imgName);
-                        const embed = new Discord.MessageEmbed()
-                            // .setTitle(user.nickname + ' Gacha Results')
-                            .setAuthor(user.nickname + ' Gacha Results', URLgambar)
-                            .setTimestamp()
+        if(args.length === 0) {
+            Object.keys(bot_config.event).forEach((key, index) => {
+                const embed = new Discord.MessageEmbed()
+                            .setTitle(bot_config.event[key].name)
+                            .setDescription('**!s multipull ' + bot_config.event[key].args + '** to pull this banner')
+                            .addField('Info :', bot_config.event[key].info)
                             .setColor(12745742)
-                            .addField('SR GET', textE)
-                            .attachFiles(attachment)
-                            .setImage('attachment://' + imgName);
+                            .setImage(bot_config.event[key].url);
 
                         message.channel.send({embed});
-                    // message.channel.send(message.guild.members.cache.get(message.author.id).nickname + ' gacha results : ', {files: [theUrl]});
-                })
-            })
-            // Jimp.read('../assets/img/gambargacha.png').then(function(image){
+            });
+            return;
+        }
+    else {
+    if(!bot_config.event[args[0]]) {
+        const embed = new Discord.MessageEmbed()
+                            .setTitle('Banner not available')
+                            .setDescription('**!s multipull** for available banner list')
+                            .setColor(12745742)
+                        message.channel.send({embed});
+        return;
+    }
+    let poolRate = bot_config.event[args[0]].rateup;
+    let images = ['./assets/img/gambargacha.png']
+    let textE = [];
+    for(let i = 0; i < 11; i++){
+    
 
-            // });
-            // console.log(theUrl);
+        if(i == 10){
+            if(bot_config.event[args[0]].guaranteed){
+                a = pullGuaranteed(poolRate, bot_config.event[args[0]].guaranteed);
+                    textE.push(util.evalRarity(a.rarity,client) + a.name);
+                    images.push(a.url);
+            }
+            else{
+                a = pullSpec(poolRate);
+                if(a.rarity == 'SR'){
+                    textE.push(util.evalRarity(a.rarity,client) + a.name);
+                }
+                images.push(a.url);
+            }
+          
             
         }
+        else{
+            a = pull(poolRate);
+            if(a.rarity == 'SR'){
+                textE.push(util.evalRarity(a.rarity,client) + a.name);
+            }
+            images.push(a.url);
+           
+  
+        }
+        // console.log(i);
+    }
+    
+    textE = (textE.length > 0 ? textE : 'n/a')
+
+    var jimps = [];
+    let theUrl = '';
+    for(let i = 0; i < images.length; i++){
+        jimps.push(jimp.read(images[i])); 
+    }
+   
+
+    Promise.all(jimps).then(function(data){
+        return Promise.all(jimps);
+    }).then(function(data){
+        data[0].composite(data[1],30,30);
+        data[0].composite(data[2],186,30);
+        data[0].composite(data[3],342,30);
+
+        data[0].composite(data[4],30,186);
+        data[0].composite(data[5],186,186);
+        data[0].composite(data[6],342,186);
+
+        data[0].composite(data[7],30,342);
+        data[0].composite(data[8],186,342);
+        data[0].composite(data[9],342,342);
+
+        data[0].composite(data[10],113,498);
+        data[0].composite(data[11],269,498);
+
+        imgName = uuid.v1() + '.png';
+        theUrl = './assets/img/your/yourgacha' + imgName;
+
+        data[0].write(theUrl, function(){
+            // console.log(message.guild.members.cache.get(message.author.id));
+            const user = message.guild.members.cache.get(message.author.id);
+            let URLgambar = 'https://cdn.discordapp.com/avatars/'+ user.user.id + '/' + user.user.avatar + '.png?size=64';
+            const attachment = new Discord
+              .MessageAttachment(theUrl, imgName);
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(bot_config.event[args[0]].name + ' Pull')
+                    .setAuthor(user.nickname + ' Gacha Results', URLgambar)
+                    .setTimestamp()
+                    .setColor(12745742)
+                    .addField('SR GET', textE)
+                    .attachFiles(attachment)
+                    .setImage('attachment://' + imgName);
+
+                message.channel.send({embed});
+            // message.channel.send(message.guild.members.cache.get(message.author.id).nickname + ' gacha results : ', {files: [theUrl]});
+        })
+    })
+            
+        }
+
+ 
         // const emoji = client.emojis.cache.get('753083854239039498');
         
 
