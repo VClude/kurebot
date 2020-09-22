4aaa4e8a85f17c5f1349af1a6995cc03a7a0f56d
Fadil Ardiansyah <v@clude.id>
2020-09-22 20:58:59 +0700

    globalization crys

--- commands/topup.js
+++ commands/topup.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'topup',
-    cooldown: '3600',
+    cooldown: '120',
     description: 'topup crystal (!s topup <amount crystal>',
     execute(message, args, client) {
         let isnew = false;
