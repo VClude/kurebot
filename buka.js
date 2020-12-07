c2850f0d157ceac4e1bddca07886a45e9a97c0a8
Fadil Ardiansyah <v@clude.id>
2020-12-07 23:03:13 +0700

    yawa

--- buka.js
+++ buka.js
@@ -12,18 +12,22 @@ const token = process.env.TOKEN || conf_token;
 
 client.on("ready", () => {
    console.log('bot is ready');
-    let channel = client.channels.cache.get('782917925257871370');
+   let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
+   for(let i = 0; i < theid.length; i++){
+    let channel = theid[i];
     const emsg = new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setTitle('Perdagangan Waifu Dibuka')
     .setImage('https://i.imgur.com/DiDNF8n.jpeg')
     .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
     channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
-     
-        client.destroy();
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true });
+   }
   
-    });
+    
+}).then(() => {
+     
+    client.destroy();
 });
 
 
--- tutup.js
+++ tutup.js
@@ -10,22 +10,27 @@ const conf_token = bot_config.bot_config.conf_token;
 const token = process.env.TOKEN || conf_token;
 
 
-client.on("ready", () => {
-   console.log('bot is ready');
-    let channel = client.channels.cache.get('782917925257871370');
-    
-    const emsg = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Perdagangan Waifu Ditutup')
-    .setImage('https://i.imgur.com/Wb0gAau.jpg')
-    .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
-    channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
-    
-        client.destroy();
-    });
 
-});
+client.on("ready", () => {
+    console.log('bot is ready');
+    let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
+    for(let i = 0; i < theid.length; i++){
+     let channel = theid[i];
+     const emsg = new Discord.MessageEmbed()
+     .setColor('#0099ff')
+     .setTitle('Perdagangan Waifu Ditutup')
+     .setImage('https://i.imgur.com/Wb0gAau.jpg')
+     .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+     channel.send(emsg);
+     channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true });
+    }
+   
+     
+ }).then(() => {
+      
+     client.destroy();
+ });
+ 
 
 
 client.login(token);
