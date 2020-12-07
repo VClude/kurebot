8ff7a61e29ed75c301aa219a262bd9e7d9c67982
Fadil Ardiansyah <v@clude.id>
2020-12-07 23:11:51 +0700

    asd

--- buka.js
+++ buka.js
@@ -9,27 +9,67 @@ const conf_token = bot_config.bot_config.conf_token;
 //Remove the OR (||) before deploying!
 const token = process.env.TOKEN || conf_token;
 
-function asd(as){
-    if(as == 4){
-        client.destroy();
-    }
-}
+
 client.on("ready", () => {
    console.log('bot is ready');
    let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
-   for(let i = 0; i < theid.length; i++){
-    let channel = client.channels.cache.get(theid[i]);
+    let channel = client.channels.cache.get(theid[0]);
     const emsg = new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setTitle('Perdagangan Waifu Dibuka')
     .setImage('https://i.imgur.com/DiDNF8n.jpeg')
     .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
     channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true });
-    asd(i);  
-}
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
+     
+        let channel = client.channels.cache.get(theid[1]);
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Dibuka')
+    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
+    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
+     
+        let channel = client.channels.cache.get(theid[2]);
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Dibuka')
+    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
+    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
+     
+        let channel = client.channels.cache.get(theid[3]);
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Dibuka')
+    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
+    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
+     
+        let channel = client.channels.cache.get(theid[4]);
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Dibuka')
+    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
+    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
+     
+        client.destroy();
   
+    });
   
-})
+    });
+  
+    });
+  
+    });
+  
+    });
+});
+
 
 client.login(token);
--- tutup.js
+++ tutup.js
@@ -9,35 +9,69 @@ const conf_token = bot_config.bot_config.conf_token;
 //Remove the OR (||) before deploying!
 const token = process.env.TOKEN || conf_token;
 
-function asd(as){
-    if(as == 4){
-        client.destroy();
-    }
-}
 
 client.on("ready", () => {
-    console.log('bot is ready');
-    let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
-    for(let i = 0; i < theid.length; i++){
-        let channel = client.channels.cache.get(theid[i]);
+   console.log('bot is ready');
+   let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
 
-     const emsg = new Discord.MessageEmbed()
-     .setColor('#0099ff')
-     .setTitle('Perdagangan Waifu Ditutup')
-     .setImage('https://i.imgur.com/Wb0gAau.jpg')
-     .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
-     channel.send(emsg);
-     channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true });
-    asd(i);
-    }
-   
+    let channel = client.channels.cache.get(theid[0]);
     
-     
- })
-      
-     
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Ditutup')
+    .setImage('https://i.imgur.com/Wb0gAau.jpg')
+    .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
+    
+        let channel = client.channels.cache.get(theid[1]);
+    
+        const emsg = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Perdagangan Waifu Ditutup')
+        .setImage('https://i.imgur.com/Wb0gAau.jpg')
+        .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+        channel.send(emsg);
+        channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
+        
+            let channel = client.channels.cache.get(theid[2]);
+    
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Ditutup')
+    .setImage('https://i.imgur.com/Wb0gAau.jpg')
+    .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
+    
+        let channel = client.channels.cache.get(theid[3]);
+    
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Ditutup')
+    .setImage('https://i.imgur.com/Wb0gAau.jpg')
+    .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
+    
+        let channel = client.channels.cache.get(theid[4]);
+    
+    const emsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Perdagangan Waifu Ditutup')
+    .setImage('https://i.imgur.com/Wb0gAau.jpg')
+    .addField('PEMBERITAHUAN', 'Buka kembali 40 Menit kedepan');
+    channel.send(emsg);
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false }).then(() => {
+    
+        client.destroy();
+    });
+    });
+    });
+        });
+    });
 
- 
+});
 
 
 client.login(token);
