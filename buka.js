aed830777c3f2432b24b568f108f74304c321cfe
Fadil Ardiansyah <v@clude.id>
2020-12-08 22:35:05 +0700

    asd

--- buka.js
+++ buka.js
@@ -12,7 +12,7 @@ const token = process.env.TOKEN || conf_token;
 
 client.on("ready", () => {
    console.log('bot is ready');
-   let theid = ['782917925257871370','785535330848997376','785535364584308766','785535392425705524','785535418422657065'];
+   let theid = ['782917925257871370','785535330848997376'];
     let channel = client.channels.cache.get(theid[0]);
     const emsg = new Discord.MessageEmbed()
     .setColor('#0099ff')
@@ -31,40 +31,7 @@ client.on("ready", () => {
     channel.send(emsg);
     channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
      
-        let channel = client.channels.cache.get(theid[2]);
-    const emsg = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Perdagangan Waifu Dibuka')
-    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
-    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
-    channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
-     
-        let channel = client.channels.cache.get(theid[3]);
-    const emsg = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Perdagangan Waifu Dibuka')
-    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
-    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
-    channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
-     
-        let channel = client.channels.cache.get(theid[4]);
-    const emsg = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Perdagangan Waifu Dibuka')
-    .setImage('https://i.imgur.com/DiDNF8n.jpeg')
-    .addField('PEMBERITAHUAN', 'Tutup Dalam 15 Menit');
-    channel.send(emsg);
-    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: true }).then(() => {
-     
         client.destroy();
-  
-    });
-  
-    });
-  
-    });
   
     });
   
